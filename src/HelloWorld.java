c9f999a3d28fe982d633ea5a7469453d19e7a10b
dk6069 <david-kniss@gmx.de>
2015-09-23 18:36:45 +0200

    Initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		while(true) {
-		System.out.println("Hello World!!!!!!!");
-		}
+		System.out.println("Hello World!");
 	}
 
 }
