c569a2f90111f5c3da2fff67dbf4730b67329646
dk6069 <david-kniss@gmx.de>
2015-10-29 20:58:23 +0100

    Commit TT

--- src/main/java/rest_calls/Application.java
+++ src/main/java/rest_calls/Application.java
@@ -2,7 +2,7 @@ package rest_calls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class Application {
 
